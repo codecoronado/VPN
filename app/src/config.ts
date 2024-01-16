54db1a0a5aa35f1cfd3fe1b4543f3aea13fdf187
basscoronado@gmail.com <Macy@1998>
2024-01-15 21:40:45 -0600

    updated with new asset

--- app/src/config.ts
+++ app/src/config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
         {
           media: {
             type: 'sticker',
-            src: import('./assets/stickers/pile-of-coins.tgs'),
+            src: import('./assets/stickers/daily-reward.tgs'),
             size: 250,
           },
           shape: 'rounded',
